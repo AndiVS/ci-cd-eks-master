3308d6c341140b0ae7212e29328de1d9cb9da83d
Andrei Saldyun <andrei.saldyun@effective-soft.com>
2022-03-01 10:12:40 +0300

    v1.0

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	// ===============================
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, fmt.Sprintf("Hello, %s", c.QueryParam("name")))
+		return c.String(http.StatusOK, fmt.Sprintf("Hello, %s", c.QueryParam("name ")))
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
